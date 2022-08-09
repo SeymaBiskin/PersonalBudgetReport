0d1d6c54232fb80954bde73a65526b48fd2daafc
Kesici <Seyma.Kesici@dnv.com>
2022-08-10 00:29:38 +0200

    Budget List class added

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,9 +1,18 @@
 import Expense
 from collections import Counter
+import matplotlib.pyplot as plt
+
+category_list = []
+top_five_list = {}
 
 def main():
 
-    category_list = []
+    def plot_data_as_bar(*args):
+        fig, ax = plt.subplots()
+        ax.bar(*args)
+        ax.set_title("# of Purchases by Category")
+        plt.show()
+
 
     expenses = Expense.Expenses()
     expenses.read_expenses("../data/spending_data.csv")
@@ -14,12 +23,13 @@ def main():
         for expense in expense_set:
             category_list.append(expense.category)
     
-    spending_categories = Counter(category_list)
-    print(spending_categories)
+    spending_counter = Counter(category_list)
+    top5 = spending_counter.most_common(5)
+    categories, count = zip(*top5)
+
+    plot_data_as_bar(categories, count)
+
    
-
-
-
 
 if __name__ == "__main__":
     main()
